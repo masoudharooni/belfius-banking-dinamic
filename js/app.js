774856ebd142e3e1ff127f9d5c8edacfd81f449e
Ahmadreza Mozaffary <79336004+AhmadrezaMozaffary@users.noreply.github.com>
2021-12-27 09:51:47 +0330

    Removing hardcoded datas

--- js/app.js
+++ js/app.js
@@ -1,81 +1,5 @@
 "use strict";
 
-// Data
-const account1 = {
-  owner: "Ahmadreza Mozaffary",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  movementsDates: [
-    "2019-11-18T21:31:17.178Z",
-    "2019-12-23T07:42:02.383Z",
-    "2020-01-28T09:15:04.904Z",
-    "2020-04-01T10:17:24.185Z",
-    "2021-09-10T14:11:59.604Z",
-    "2021-09-13T17:01:17.194Z",
-    "2021-09-15T23:36:17.929Z",
-    "2021-09-16T10:51:36.790Z",
-  ],
-  interestRate: 1.2, // %
-  pin: 1111,
-  currency: "EUR",
-  locale: "en-US",
-};
-
-const account2 = {
-  owner: "Mohammad Alavi",
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  movementsDates: [
-    "2019-11-01T13:15:33.035Z",
-    "2019-11-30T09:48:16.867Z",
-    "2019-12-25T06:04:23.907Z",
-    "2020-01-25T14:18:46.235Z",
-    "2020-02-05T16:33:06.386Z",
-    "2020-04-10T14:43:26.374Z",
-    "2020-06-25T18:49:59.371Z",
-    "2020-07-26T12:01:20.894Z",
-  ],
-  interestRate: 1.5,
-  pin: 2222,
-  currency: "EUR",
-  locale: "pt-PT",
-};
-
-const account3 = {
-  owner: "Bijan mahdavi Tehrani",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  movementsDates: [
-    "2019-11-01T13:15:33.035Z",
-    "2019-11-30T09:48:16.867Z",
-    "2019-12-25T06:04:23.907Z",
-    "2020-01-25T14:18:46.235Z",
-    "2020-02-05T16:33:06.386Z",
-    "2020-04-10T14:43:26.374Z",
-    "2020-06-25T18:49:59.371Z",
-    "2020-07-26T12:01:20.894Z",
-  ],
-  interestRate: 0.7,
-  pin: 3333,
-  currency: "IRR",
-  locale: "fa-IR",
-};
-
-const account4 = {
-  owner: "Sara Nori",
-  movements: [430, 1000, 700, 50, 90],
-  movementsDates: [
-    "2019-11-18T21:31:17.178Z",
-    "2019-12-23T07:42:02.383Z",
-    "2020-01-28T09:15:04.904Z",
-    "2020-04-01T10:17:24.185Z",
-    "2020-05-08T14:11:59.604Z",
-  ],
-  interestRate: 1,
-  pin: 4444,
-  currency: "USD",
-  locale: "de-DE",
-};
-
-const accounts = [account1, account2, account3, account4];
-
 //Elements
 const inputUser = document.querySelector(".username");
 const inputPass = document.querySelector(".pass");
@@ -202,17 +126,6 @@ const calcDisplaySummary = function (acc) {
     acc.currency
   )}`;
 };
-
-const accUsernames = function (accs) {
-  accs.forEach(function (acc) {
-    acc.username = acc.owner
-      .toLowerCase()
-      .split(" ")
-      .map((name) => name[0])
-      .join("");
-  });
-};
-accUsernames(accounts);
 
 const updateUI = function (acc) {
   displayMovements(acc);
